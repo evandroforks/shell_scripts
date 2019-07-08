4cf53cfc1a0cfd9128c9f8cb6b08a50d6e69b069
evandrocoan <evandrocoan@hotmail.com>
2019-07-08 17:18:04 -0300

    Update timer_calculator.sh

--- timer_calculator.sh
+++ timer_calculator.sh
@@ -1,50 +1,70 @@
-#!/bin/sh
-
+#!/bin/bash
+#
+# time_calculator.sh
+# Copyright (c) 2019 Evandro Coan
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
 
 # The time flag file path
-updateFlagFilePath="/tmp/.time_flag.txt"
+updateFlagFilePath="/$(pwd)/.time_flag.txt";
 
 # Save the current seconds, only if it is not already saved
-if ! [ -f $updateFlagFilePath ]
+if ! [ -f "$updateFlagFilePath" ];
 then
     # Create a flag file to avoid override the initial time and save it.
-    printf "%s" "$(date +%s.%N)" > $updateFlagFilePath
+    printf "%s" "$(date +%s.%N)" > "$updateFlagFilePath";
 
-    # printf "$1\\n"
-    printf "Current time: %s\\n" "$(date)"
-fi
+    # printf "$1\\n";
+    printf "Current time: %s\\n" "$(date)";
+fi;
 
 # Clean the flag file
 cleanUpdateFlagFile()
 {
-    if [ -f $updateFlagFilePath ]
+    if [ -f "$updateFlagFilePath" ];
     then
-        cat $updateFlagFilePath
-        rm $updateFlagFilePath
-    fi
+        cat "$updateFlagFilePath"
+        rm "$updateFlagFilePath";
+    fi;
 }
 
 # Calculates and prints to the screen the seconds elapsed since this script started.
 showTheElapsedSeconds()
 {
     # Clean the flag file and read the time
-    scriptStartSecond=$(cleanUpdateFlagFile)
+    scriptStartSecond=$(cleanUpdateFlagFile);
 
     # Calculates whether the seconds program parameter is an integer number
-    isFloatNumber "$scriptStartSecond"
+    isFloatNumber "$scriptStartSecond";
 
     # `$?` captures the return value of the previous function call command
     # Print help when it is not passed a second command line argument integer
-    if [ $? -eq 1 ]
+    if [ "$?" -eq 1 ];
     then
-        scripExecutionTimeResult=$(awk "BEGIN {printf \"%.2f\",$(date +%s.%N)-$scriptStartSecond}")
+        scripExecutionTimeResult=$(awk "BEGIN {printf \"%.2f\",$(date +%s.%N)-$scriptStartSecond}");
         integer_time="$(float_to_integer "$scripExecutionTimeResult")"
 
-        printf "Took '%s' " "$(convert_seconds "$integer_time" "$scripExecutionTimeResult")"
-        printf "seconds to run the script, %s.\\n" "$(date +%H:%M:%S)"
+        printf "Took '%s' " "$(convert_seconds "$integer_time" "$scripExecutionTimeResult")";
+        printf "seconds to run the script, %s.\\n" "$(date +%H:%M:%S)";
     else
-        printf "Could not calculate the seconds to run '%s'.\\n" "$1"
-    fi
+        printf "Could not calculate the seconds to run '%s'.\\n" "$1";
+    fi;
 }
 
 # Convert seconds to hours, minutes, seconds, milliseconds
@@ -54,8 +74,8 @@ showTheElapsedSeconds()
 # https://unix.stackexchange.com/questions/131073/awk-printf-number-in-width-and-round-it-up
 convert_seconds()
 {
-    # printf "$1$2\n"
-    printf "%s %s" "$1" "$2" | awk '{printf("%d:%02d:%02d:%02d.%02.0f", ($1/60/60/24), ($1/60/60%24), ($1/60%60), ($1%60), (($2-$1)*100))}'
+    # printf "$1$2\n";
+    printf "%s %s" "$1" "$2" | awk '{printf("%d:%02d:%02d:%02d.%02.0f", ($1/60/60/24), ($1/60/60%24), ($1/60%60), ($1%60), (($2-$1)*100))}';
 }
 
 # Bash: Float to Integer
@@ -63,7 +83,7 @@ convert_seconds()
 float_to_integer()
 {
     awk 'BEGIN{for (i=1; i<ARGC;i++)
-        printf "%.0f\\n", ARGV[i]}' "$@"
+        printf "%.0f\\n", ARGV[i]}' "$@";
 }
 
 # Determine whether its first parameter is empty or not.
@@ -71,12 +91,12 @@ float_to_integer()
 # Returns 1 if empty, otherwise returns 0.
 isEmpty()
 {
-    if [ -z ${1+x} ]
+    if [ -z ${1+x} ];
     then
-        return 1
-    fi
+        return 1;
+    fi;
 
-    return 0
+    return 0;
 }
 
 
@@ -86,19 +106,19 @@ isEmpty()
 isInteger()
 {
     # Calculates whether the first function parameter $1 is a number
-    isEmpty "$1"
+    isEmpty "$1";
 
     # `$?` captures the return value of the previous function call command
     # Notify an invalid USB port number passed as parameter.
-    if ! [ $? -eq 1 ]
+    if ! [ $? -eq 1 ];
     then
-        if [ "$1" -eq "$1" ] 2>/dev/null
+        if [ "$1" -eq "$1" ] 2>/dev/null;
         then
-            return 1
-        fi
-    fi
+            return 1;
+        fi;
+    fi;
 
-    return 0
+    return 0;
 }
 
 
@@ -108,33 +128,32 @@ isInteger()
 isFloatNumber()
 {
     # Calculates whether the first function parameter $1 is a number
-    isEmpty "$1"
+    isEmpty "$1";
 
     # `$?` captures the return value of the previous function call command
     # Notify an invalid USB port number passed as parameter.
-    if ! [ $? -eq 1 ]
+    if ! [ $? -eq 1 ];
     then
         # Removed the file extension, just in case there exists.
-        firstFloatNumberPart=$(printf "%s" "$1" | cut -d'.' -f 1)
-        secondFloatNumberPart=$(printf "%s" "$1" | cut -d'.' -f 2)
+        firstFloatNumberPart=$(printf "%s" "$1" | cut -d'.' -f 1);
+        secondFloatNumberPart=$(printf "%s" "$1" | cut -d'.' -f 2);
 
         # Checks whether the first float number part is an integer.
-        isInteger "$firstFloatNumberPart"
+        isInteger "$firstFloatNumberPart";
 
-        if ! [ $# -eq 1 ]
+        if ! [ $# -eq 1 ];
         then
-            return 0
-        fi
+            return 0;
+        fi;
 
         # Checks whether the second float number part is an integer.
-        isInteger "$secondFloatNumberPart"
+        isInteger "$secondFloatNumberPart";
 
-        if [ $# -eq 1 ]
+        if [ $# -eq 1 ];
         then
-            return 1
-        fi
-    fi
+            return 1;
+        fi;
+    fi;
 
-    return 0
+    return 0;
 }
-
